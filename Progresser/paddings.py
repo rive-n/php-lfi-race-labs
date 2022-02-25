852161090dad6abc7e379e8c7b5cbc5345d0d2f9
rive_n <45512613+rive-n@users.noreply.github.com>
2022-02-25 23:36:35 +0300

    Update paddings.py

--- Progresser/paddings.py
+++ Progresser/paddings.py
@@ -30,8 +30,7 @@ from string import ascii_letters as letters
 st = "upload_progress_t_"
 st_len = len(st)
 for i in range(300):
-    if i % 8 == 0 and i * 8 % 6 != 0:
-        if i - 2> st_len:
-            to_enc =  st + "".join(choice(letters) for _ in range(st_len, i - 2))
-            if b"=" not in encode(to_enc.encode()) and b"=" not in encode(encode(to_enc.encode())) and b"=" not in encode(encode(encode(to_enc.encode()))):
-                print(f"[+] {to_enc}")
+    if (i % 9 == 0 or i % 3 == 0) and i - 2> st_len:
+        to_enc =  st + "".join(choice(letters) for _ in range(st_len, i - 2))
+        if b"=" not in encode(to_enc.encode()) and b"=" not in encode(encode(to_enc.encode())) and b"=" not in encode(encode(encode(to_enc.encode()))):
+            print(f"[+] {to_enc}")
